27e7c9e434f6853a8e478ea87d77c15ad2f05a20
tkmagesh <tkmagesh77@gmail.com>
2016-03-30 15:54:04 +0530

    Handling get requests for dynamic resources

--- Day-01/06-webserver/server.js
+++ Day-01/06-webserver/server.js
@@ -1,26 +1,38 @@
 var http = require('http'),
 	path = require('path'),
-	fs = require('fs');
+	fs = require('fs'),
+	querystring = require('querystring'),
+	url = require('url'),
+	calculator = require('./calculator');
 
-/*
-res.statusCode = 404;
-res.end();
-
-*/
+var staticExtns = ['.html', '.js', '.png', '.ico', '.json', '.xml'];
+function isStatic(resource){
+	return staticExtns.indexOf(path.extname(resource)) !== -1;
+}
 var server = http.createServer(function(req, res){
-	var resource = path.join(__dirname, req.url);
-	if (!fs.existsSync(resource)){
+	var urlObj = url.parse(req.url);
+	var resource = path.join(__dirname, urlObj.pathname);
+	if (isStatic(resource)){
+		if (!fs.existsSync(resource)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		var stream = fs.createReadStream(resource);
+		stream.pipe(res);
+	} else if (urlObj.pathname === '/calculator'){
+		var input = querystring.parse(urlObj.query);
+		var n1 = parseInt(input.n1, 10),
+			n2 = parseInt(input.n2, 10),
+			op = input.op;
+
+		var result = calculator[op](n1, n2);
+		res.write(result.toString());
+		res.end();
+	} else {
 		res.statusCode = 404;
 		res.end();
-		return;
 	}
-	var stream = fs.createReadStream(resource);
-	stream.on('data', function(chunk){
-		res.write(chunk);
-	});
-	stream.on('end', function(){
-		res.end();
-	});
 });
 
 server.listen(8080);
