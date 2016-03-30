bf3df1901a804e1a4da8ed5170bae8428a61624e
tkmagesh <tkmagesh77@gmail.com>
2016-03-30 16:00:01 +0530

    Handling post requests

--- Day-01/06-webserver/server.js
+++ Day-01/06-webserver/server.js
@@ -20,7 +20,7 @@ var server = http.createServer(function(req, res){
 		}
 		var stream = fs.createReadStream(resource);
 		stream.pipe(res);
-	} else if (urlObj.pathname === '/calculator'){
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
 		var input = querystring.parse(urlObj.query);
 		var n1 = parseInt(input.n1, 10),
 			n2 = parseInt(input.n2, 10),
@@ -29,6 +29,22 @@ var server = http.createServer(function(req, res){
 		var result = calculator[op](n1, n2);
 		res.write(result.toString());
 		res.end();
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var reqData = '';
+		req.on('data', function(chunk){
+			reqData += chunk;
+		});
+		req.on('end', function(){
+			console.log('incoming data -> ', reqData);
+			var input = querystring.parse(reqData);
+			var n1 = parseInt(input.n1, 10),
+				n2 = parseInt(input.n2, 10),
+				op = input.op;
+
+			var result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();	
+		});
 	} else {
 		res.statusCode = 404;
 		res.end();
