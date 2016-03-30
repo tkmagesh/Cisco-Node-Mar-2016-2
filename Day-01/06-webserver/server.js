de9e2b497584f7dc07f23c01b40dd70c24ab76f3
tkmagesh <tkmagesh77@gmail.com>
2016-03-30 15:10:55 +0530

    static web server

--- Day-01/06-webserver/server.js
+++ Day-01/06-webserver/server.js
@@ -1,4 +1,6 @@
-var http = require('http');
+var http = require('http'),
+	path = require('path'),
+	fs = require('fs');
 
 /*
 res.statusCode = 404;
@@ -6,9 +8,19 @@ res.end();
 
 */
 var server = http.createServer(function(req, res){
-	console.log('a new connection is established for - ', req.url);
-	res.write('<h1>Welcome to node.js</h1>');
-	res.end();
+	var resource = path.join(__dirname, req.url);
+	if (!fs.existsSync(resource)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	var stream = fs.createReadStream(resource);
+	stream.on('data', function(chunk){
+		res.write(chunk);
+	});
+	stream.on('end', function(){
+		res.end();
+	});
 });
 
 server.listen(8080);
