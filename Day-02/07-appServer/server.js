615f9bd87804e0dc2235eae82560a2fae15cec34
tkmagesh <tkmagesh77@gmail.com>
2016-03-31 11:29:16 +0530

    introducing app

--- Day-02/07-appServer/server.js
+++ Day-02/07-appServer/server.js
@@ -3,22 +3,14 @@ var http = require('http'),
 	dataParser = require('./dataParser.js'),
 	serveStatic = require('./serveStatic.js'),
 	calculatorReqHandler = require('./calculatorReqHandler.js'),
-	notFoundHandler = require('./notFoundHandler.js');
+	notFoundHandler = require('./notFoundHandler.js'),
+	app = require('./app');
 
-var middlewares = [logger, dataParser, serveStatic, calculatorReqHandler, notFoundHandler];
+app.use(logger);
+app.use(dataParser);
+app.use(serveStatic);
+app.use(calculatorReqHandler);
+app.use(notFoundHandler);
 
-function exec(middlewares, req, res){
-	var first = middlewares[0],
-		remaining = middlewares.slice(1),
-		next = function(){
-			exec(remaining, req, res);
-		};
-		if (typeof first === 'function')
-			first(req, res, next);
-}
-var server = http.createServer(function(req, res){
-	exec(middlewares, req, res);
-});
-
-server.listen(8080);
+http.createServer(app).listen(8080);
 console.log('Server listening on port 8080');
